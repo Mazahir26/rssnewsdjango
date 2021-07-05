9b5c96f18bf4e85f5093660c42b1abf8fa71cd79
Mazahir26 <supermazahir@gmail.com>
2021-07-05 17:40:51 +0530

    deploy try 004

--- Rss/settings.py
+++ Rss/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import os
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -129,3 +130,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+django_heroku.settings(locals())
